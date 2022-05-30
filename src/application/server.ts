495e526edfd00526d737014195673f6fef3ddd77
Henrique Balmant <henrique.balmant@gmail.com>
2022-05-30 16:28:28 -0300

    docs(swagger): add swagger support
    Add Swagger support for OpenAPI 3.0.0

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "@docs/swagger.json";
+
 const app = express();
+
+app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get('/', (request, response) => response.json({ message: 'hello world' }));
 
-app.listen(3333);
-
-console.log('teste')
+app.listen(3333, () => {
+  console.log("🚀 Server is running on port 3333...");
+});
